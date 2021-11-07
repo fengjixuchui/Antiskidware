e8b30901afbf9977947118087fdd1b82bd5f5e94
Ѵιcнч <66045929+Its-Vichy@users.noreply.github.com>
2021-11-07 05:12:46 +0100

    🕶️ Fix missing dll error
    - https://www.it-swarm-fr.com/fr/c%2B%2B/libstdc-6.dll-introuvable/972632720/

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ echo Building boot binary...
 nasm -f bin -o dist/boot.bin boot.asm
 
 echo Building Exe..
-g++ main.cpp -o dist/Antiskidware.exe
+g++ main.cpp -o dist/Antiskidware.exe -static-libgcc -static-libstdc++
 
 echo Finished.
 pause
