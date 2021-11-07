8f3ba3913ede2cb1d20595ca37f2409190224599
Ѵιcнч <66045929+Its-Vichy@users.noreply.github.com>
2021-11-07 07:02:27 +0100

    😆 Need fix: "'thread' is not a member of 'std'"

--- build.bat
+++ build.bat
@@ -7,8 +7,9 @@ mkdir dist
 echo Building boot binary...
 nasm -f bin -o dist/boot.bin boot.asm
 
-echo Building Exe..
-g++ main.cpp -o dist/Antiskidware.exe -static-libgcc -static-libstdc++
+:: "error: 'thread' is not a member of 'std'"
+:: echo Building Exe..
+:: g++ main.cpp -o dist/Antiskidware.exe -static-libgcc -static-libstdc++
 
 echo Finished.
 pause
