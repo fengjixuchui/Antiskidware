0756bfc3b6fc8c718abe735476f721ad4117840d
Ѵιcнч <66045929+Its-Vichy@users.noreply.github.com>
2021-11-07 03:27:50 +0100

    👀 Look to understand

---
+++ main.cpp
@@ -0,0 +1,5 @@
+/*
+    Ref:
+      - Overwrite MBR and replace boot sector with custom bin: https://github.com/sheep-o/Windows-MBR-Overwrite
+      - Download nasm: https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/win64/
+*/
